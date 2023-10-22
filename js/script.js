4d6188df0e69ebfd591d56d9764556223adeb3ee
DenisIT <denis.d88@bk.ru>
2023-10-22 23:51:54 +0700

    commit №2

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ open_nav.addEventListener('click', function(){
 		nav_list.classList.remove('nav_list_item_show')
 	}
 })
+
+//Обработчик формы
+let regForm = document.getElementById('formReg');
+let showData = document.querySelector('.showData')
+let regEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+let messageError = document.querySelector('.messageError');
+
+regForm.addEventListener('submit', function(e){
+	if (!regEmail.test(regForm.email.value)){
+		messageError.innerHTML = 'Ошибка в Email'
+		e.preventDefault();
+	}
+	else if (regEmail.test(regForm.email.value) == true) {
+		regForm.onsubmit();
+	}
+})
